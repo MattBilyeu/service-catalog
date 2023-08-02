f7b8c30e611ee3bac03b9c03a0062a06a910628a
Matt Bilyeu <matt.bilyeu1@gmail.com>
2023-08-02 17:01:31 -0400

    Rough Completion
    Overall look is good, need to update colors once the color palette is finally decided upon.
    
    Also need to update the contentful stuff once that account has been created.  Currently it loads hard-coded content.

--- src/app/packages/packages.component.ts
+++ src/app/packages/packages.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from '../shared/contentful.service';
 
 @Component({
   selector: 'app-packages',
   templateUrl: './packages.component.html',
   styleUrls: ['./packages.component.css']
 })
-export class PackagesComponent {
+export class PackagesComponent implements OnInit {
+  packages: any;
+
+  constructor(private contentful: ContentfulService) {}
+
+  ngOnInit() {
+    this.packages = this.contentful.packages;
+  }
 
 }
--- src/app/services/service-item/service-item.component.ts
+++ src/app/services/service-item/service-item.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ContentfulService } from 'src/app/shared/contentful.service';
 
 @Component({
   selector: 'app-service-item',
   templateUrl: './service-item.component.html',
   styleUrls: ['./service-item.component.css']
 })
-export class ServiceItemComponent {
+export class ServiceItemComponent implements OnInit {
+  entry: any;
+
+  constructor(private contentful: ContentfulService,
+              private router: Router,
+              private route: ActivatedRoute) {}
+  
+  ngOnInit() {
+    const params = this.route.params.subscribe(
+      (params: Params) => {
+        this.entry = this.contentful.entries[params['id']]
+      }
+    )
+  }
 
 }
